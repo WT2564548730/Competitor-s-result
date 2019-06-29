6e6d88ef1390bdb5851609a6c99f2d6dd89e5211
Tao2564548730 <2564548730@qq.com>
2019-06-29 14:13:42 +0800

    Update main.cpp

--- Competitor's result/main.cpp
+++ Competitor's result/main.cpp
@@ -6,30 +6,32 @@ ifstream win("/Users/WT20181105881/Desktop/tin.txt");
 ifstream jin("/Users/WT20181105881/Desktop/fin.txt");
 ofstream wout("/Users/WT20181105881/Desktop/tout.txt");
 
+int k=1;
 class Person{
-    string school,num,name,judge[7];
+    string school,num,name;
     double score[7]={0};
 public:
     double sum=0,max=0,min=0,average=0;
     Person(string s,string nu,string na);
     ~Person();
-    void display();
-    
+    void display(string judge[]);
 };
 
-void Person::display(){
+void Person::display(string judge[]){
+    wout<<"第"<<k<<"名"<<endl;
     wout<<school<<" "<<num<<" "<<name<<endl;
     for(int i=0;i<7;i++){
         wout<<judge[i]<<" "<<score[i];
         if(i!=6)    wout<<" ";
     }
     wout<<endl;
-        wout<<name<<"的平均分为"<<average<<endl;
+    wout<<name<<"的平均分为"<<average<<"分"<<endl;
+    wout<<endl;
+    k++;
 }
 
 Person::Person(string s,string nu,string na):school(s),num(nu),name(na){
     for(int i=0;i<7;i++){
-        jin>>judge[i];
         jin>>score[i];
         sum+=score[i];
         if(i==0){
@@ -47,15 +49,16 @@ Person::Person(string s,string nu,string na):school(s),num(nu),name(na){
 
 int main(){
     int n;
-    string school,num,name;
+    string school,num,name,judge[7];
     win>>n;
     Person *per[n];
     
+    for(int i=0;i<7;i++)
+        jin>>judge[i];
     for(int i=0;i<n;i++){
         win>>school>>num>>name;
         per[i] = new Person(school,num,name);
     }
-    
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-1;j++){
             if(per[j]->average<per[j+1]->average){
@@ -65,8 +68,6 @@ int main(){
             }
         }
     }
-    
-    for(int i=0;i<n;i++){
-        per[i]->display();
-    }
+    for(int i=0;i<n;i++)
+        per[i]->display(judge);
 }
